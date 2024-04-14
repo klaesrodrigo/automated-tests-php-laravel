bfe5d97ff67f6bb423b803f969d62dfe17a1ed2d
Rodrigo Klaes <rodrigoklaes@gmail.com>
2024-04-14 17:11:43 -0300

    testing store task

--- tests/Feature/StoreTaskTest.php
+++ tests/Feature/StoreTaskTest.php
@@ -8,15 +8,53 @@ class StoreTaskTest extends TestCase
 {
     public function testStoreTask()
     {
+        $name = 'Task 1';
+        $description = 'Description 1';
+
         $response = $this->postJson('/api/tasks', [
-            'name' => 'Task 1',
-            'description' => 'Description 1',
+            'name' => $name,
+            'description' => $description,
         ]);
 
         $response->assertStatus(201)
             ->assertJson([
-                'name' => 'Task 1',
-                'description' => 'Description 1',
+                'name' => $name,
+                'description' => $description,
             ]);
+    }
+
+    public function testStoreTaskAndCheckIfItWasSaved()
+    {
+        $name = 'Task 2';
+        $description = 'Description 2';
+
+        $response = $this->postJson('/api/tasks', [
+            'name' => $name,
+            'description' => $description,
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'name' => $name,
+                'description' => $description,
+            ]);
+
+        $task = $response->json();
+        $this->assertDatabaseHas('tasks', [
+            'id' => $task['id'],
+            'name' => $task['name'],
+            'description' => $task['description'],
+        ]);
+    }
+
+    public function testStoreTaskWithInvalidData()
+    {
+        $response = $this->postJson('/api/tasks', [
+            'name' => '',
+            'description' => '',
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['name', 'description']);
     }
 }
