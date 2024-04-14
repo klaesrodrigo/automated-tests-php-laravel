6e28bc60b8901bc5c2e5319a72965ab203c0a3e1
Rodrigo Klaes <rodrigoklaes@gmail.com>
2024-04-13 22:01:57 -0300

    routing index task

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IndexTaskController;
 use App\Http\Controllers\StoreTaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/tasks', StoreTaskController::class);
+Route::get('/tasks', IndexTaskController::class);
