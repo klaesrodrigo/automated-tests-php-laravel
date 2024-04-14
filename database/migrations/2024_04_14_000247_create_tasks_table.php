74f5ef7ec838e0fbc43ada2ccd8f240c00fb52ae
Rodrigo Klaes <rodrigoklaes@gmail.com>
2024-04-13 21:48:48 -0300

    some issues revolved

--- database/migrations/2024_04_14_000247_create_tasks_table.php
+++ database/migrations/2024_04_14_000247_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
